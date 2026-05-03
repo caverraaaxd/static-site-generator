ffed8a137246c309051b5feb2e399e66f6c7f9e3
caverraaaxd <caverra4love@gmail.com>
2026-05-03 17:23:41 +0000

    feat: deploy with github pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -15,7 +15,7 @@ def extract_title(markdown):
 
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r") as f:
@@ -33,6 +33,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content_html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok= True)
 
@@ -40,12 +42,12 @@ def generate_page(from_path, template_path, dest_path):
         f.write(template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for item in os.listdir(dir_path_content):
         item_path = os.path.join(dir_path_content, item)
         if os.path.isfile(item_path):
             dest_path = os.path.join(dest_dir_path, item)
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(item_path, template_path, dest_path)
+            generate_page(item_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(item_path, template_path, os.path.join(dest_dir_path, item))
+            generate_pages_recursive(item_path, template_path, os.path.join(dest_dir_path, item), basepath)
--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
 import os
+import sys
 from textnode import TextNode, TextType
 from copystatic import copy_files_recursive, folder_cleanup
 from gencontent import generate_page, generate_pages_recursive
 
 
 DIR_PATH_STATIC = "./static"
-DIR_PATH_PUBLIC = "./public"
+DIR_PATH_PUBLIC = "./docs"
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
     folder_cleanup(DIR_PATH_PUBLIC)
     copy_files_recursive(DIR_PATH_STATIC, DIR_PATH_PUBLIC)
-    generate_pages_recursive("content/", "template.html", "public/")
+    generate_pages_recursive("content/", "template.html", "public/", basepath)
+    
+    
 
 
 if __name__ == "__main__":
